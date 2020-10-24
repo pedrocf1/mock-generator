ec9ad9505c0a50c65bc8e3e35e732e3a14e9d362
pcf <pcf@cesar.org.br>
2020-10-23 21:53:49 -0300

    cleaning code

--- src/app/mock-form/mock-field/mock-field.component.ts
+++ src/app/mock-form/mock-field/mock-field.component.ts
@@ -1,62 +1,64 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MockField } from '../model/mock-field';
 import { FieldType } from '../model/fieldType.interface';
 import { MockFormService } from '../mock-form.service';
 import { Types } from '../model/types.enum';
-import { runInThisContext } from 'vm';
 
 @Component({
-  selector: 'mock-field',
+  selector: 'app-mock-field',
   templateUrl: './mock-field.component.html',
   styleUrls: ['./mock-field.component.css']
 })
 export class MockFieldComponent implements OnInit {
 
-  @Input() mockField: MockField
-  @Input() isChild: boolean = false
-  @Input() parentType: string = ""
-  
-  hasName:boolean = true
-  fieldTypes:Array<FieldType>
+  @Input() mockField: MockField;
+  @Input() isChild: boolean;
+  @Input() parentType: string;
+  hasName: boolean;
+  fieldTypes: Array<FieldType>;
 
-  isCollapsed:number = Math.floor(Math.random() * 6) + 1  
+  isCollapsed: number = Math.floor(Math.random() * 6) + 1;
 
-  constructor(private _mockFormService:MockFormService) { }
+  constructor(private mockFormService: MockFormService) {
+    this.isChild = false;
+    this.parentType = '';
+    this.hasName = true;
+  }
 
   ngOnInit() {
-    this.fieldTypes = this._mockFormService.fieldTypes()
-    this.decideIfHasName()
+    this.fieldTypes = this.mockFormService.fieldTypes();
+    this.decideIfHasName();
   }
 
-  addField(){
-    this.mockField.children.push(new MockField(null))
+  addField() {
+    this.mockField.children.push(new MockField(null));
   }
 
-  private decideIfHasName():void{
-    
-    if(this.parentType === Types.Object){
-      this.hasName = true;
-      return;
-    }else if(this.parentType === Types.Array){
+  private decideIfHasName(): void {
+    if (this.parentType === Types.Object || this.parentType === Types.Array) {
       this.hasName = true;
       return;
     }
+    // else if(this.parentType === Types.Array){
+    //   this.hasName = true;
+    //   return;
+    // }
 
     // if(this.mockField.fieldType === Types.Object && this.isChild){
     //   this.hasName = true;
     //   return;
     // }
 
-    if(this.isComplexType()){
-      this.hasName = false;
-    }else if(this.mockField.fieldType===Types.Array){
+    if (this.isComplexType() || this.mockField.fieldType === Types.Array) {
       this.hasName = false;
     }
+    // else if(this.mockField.fieldType === Types.Array) {
+    //   this.hasName = false;
+    // }
 
   }
 
-  private isComplexType():boolean{
-    return this.mockField.fieldType===Types.Array || this.mockField.fieldType===Types.Object
+  private isComplexType(): boolean {
+    return this.mockField.fieldType === Types.Array || this.mockField.fieldType === Types.Object;
   }
-
 }
