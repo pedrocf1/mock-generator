cc81e32fa6cddb21881f2db19ad7eb49263d2c88
pcf <pcf@cesar.org.br>
2020-10-27 09:10:13 -0300

    cleaning code

--- src/app/mock-form/mock-form.component.ts
+++ src/app/mock-form/mock-form.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Type } from '@angular/core';
-import { FieldType } from './model/fieldType.interface';
 import { MockField } from './model/mock-field';
 import { MockBody } from './model/mock-body';
 import { Types } from './model/types.enum';
@@ -14,102 +13,99 @@ import { Constants } from './model/Constants';
 })
 export class MockFormComponent implements OnInit {
 
-  mockBody:MockBody
-  finalObj=undefined 
+  mockBody: MockBody;
+  finalObj = undefined;
 
-  constructor(private _mockFormService:MockFormService){
+  constructor(private mockFormService: MockFormService) {
 
   }
 
 
   ngOnInit(): void {
-    this.initialize()
+    this.initialize();
   }
 
-  initialize(){
-    this.mockBody = new MockBody(null)
-    this.addField()
+  initialize() {
+    this.mockBody = new MockBody(null);
+    this.addField();
   }
 
-  addField(){
-    this.mockBody.fields.push(new MockField(null))
+  addField() {
+    this.mockBody.fields.push(new MockField(null));
   }
 
-  generateMock(){
+  generateMock() {
     this.finalObj = {}
-    console.log("this.mockBody", this.mockBody)
+    console.log("this.mockBody", this.mockBody);
     this.mockBody.fields.forEach(mockField => {
-      console.log("mockField",mockField)
+      console.log("mockField", mockField);
 
-      this.createMockField(mockField)
+      this.createMockField(mockField);
 
 
     })
     console.log("OBJ GERADO", this.finalObj)
   }
 
-  private createMockField(mockField){
+  private createMockField(mockField) {
 
     switch (mockField.fieldType) {
       case Types.Object:
-        this.createObjectField(mockField)
-      break;
-    
+        this.createObjectField(mockField);
+        break;
+
       case Types.Array:
-        this.createArrayField(mockField)
-      break;
-      
+        this.createArrayField(mockField);
+        break;
+
       default:
-        this.finalObj[mockField.name] = this._mockFormService.generateMockFieldValue(mockField.fieldType, mockField.optionFields)
-      break;
+        this.finalObj[mockField.name] = this.mockFormService.generateMockFieldValue(mockField.fieldType, mockField.optionFields);
+        break;
     }
 
   }
 
-  private createObjectField(mockField:MockField){
-    this.finalObj[mockField.name] = this._mockFormService.generateMockFieldValue(mockField.fieldType, mockField.optionFields)
+  private createObjectField(mockField: MockField) {
+    this.finalObj[mockField.name] = this.mockFormService.generateMockFieldValue(mockField.fieldType, mockField.optionFields);
 
-    mockField.children.forEach(child=>{
-      switch (Types.Object) {
-        case child.fieldType:
-          this.finalObj[mockField.name][child.name] = this._mockFormService.generateMockFieldValue(child.fieldType, child.optionFields)  
-        break;
+    mockField.children.forEach(child => {
+      if (Types.Object === child.fieldType) {
+        this.finalObj[mockField.name][child.name] = this.mockFormService.generateMockFieldValue(child.fieldType, child.optionFields);
 
-        default:
-          this.createMockField(child)
-        break;
+      } else {
+        this.createMockField(child);
       }
-    })
+    });
 
   }
 
-  private createArrayField(mockField:MockField){
-    this.finalObj[mockField.name] = this._mockFormService.generateMockFieldValue(mockField.fieldType, mockField.optionFields)
-    console.log("mockField.optionFields[Constants.MIN_RANGE]",mockField.optionFields[Constants.MIN_RANGE])
-    mockField.children.forEach(child=>{
+  private createArrayField(mockField: MockField) {
+    this.finalObj[mockField.name] = this.mockFormService.generateMockFieldValue(mockField.fieldType, mockField.optionFields);
+    console.log("mockField.optionFields[Constants.MIN_RANGE]", mockField.optionFields[Constants.MIN_RANGE]);
+    mockField.children.forEach(child => {
       console.log("CHILD", child)
-        for (let index = 0; index < mockField.optionFields[Constants.MIN_RANGE]; index++) {
-          if (child.fieldType !== String(Types.Object) && child.fieldType !== String(Types.Array)) {
-            console.log("this.finalObj[mockField.name][index]", this.finalObj[mockField.name][index])
-            if(this.finalObj[mockField.name][index] === undefined){
-              this.finalObj[mockField.name][index] = {}
-            }
-            this.finalObj[mockField.name][index][child.name]=this._mockFormService.generateMockFieldValue(child.fieldType, child.optionFields)
-            
-          }else if(child.fieldType === String(Types.Object)){
-            this.finalObj[mockField.name].push(this._mockFormService.generateMockFieldValue(child.fieldType, child.optionFields))
-            const arrayleng = this.finalObj[mockField.name].length-1
-            child.children.forEach(newChild=>{
-              this.finalObj[mockField.name][arrayleng][newChild.name] = this._mockFormService.generateMockFieldValue(newChild.fieldType, newChild.optionFields)
-            })
-            
-          }else{
-            this.finalObj[mockField.name].push(this._mockFormService.generateMockFieldValue(child.fieldType, child.optionFields))
-
+      for (let index = 0; index < mockField.optionFields[Constants.MIN_RANGE]; index++) {
+        if (child.fieldType !== String(Types.Object) && child.fieldType !== String(Types.Array)) {
+          console.log("this.finalObj[mockField.name][index]", this.finalObj[mockField.name][index]);
+          if (this.finalObj[mockField.name][index] === undefined) {
+            this.finalObj[mockField.name][index] = {};
           }
+          this.finalObj[mockField.name][index][child.name] = this.mockFormService.generateMockFieldValue(child.fieldType, child.optionFields);
+
+        } else if (child.fieldType === String(Types.Object)) {
+          this.finalObj[mockField.name].push(this.mockFormService.generateMockFieldValue(child.fieldType, child.optionFields));
+          const arrayleng = this.finalObj[mockField.name].length - 1;
+          child.children.forEach(newChild => {
+            this.finalObj[mockField.name][arrayleng][newChild.name] = this.mockFormService.generateMockFieldValue(newChild.fieldType, newChild.optionFields);
+          });
+
+        } else {
+          this.finalObj[mockField.name].push(this.mockFormService.generateMockFieldValue(child.fieldType, child.optionFields));
+
         }
-      
-    })
+      }
+
+    });
 
   }
 
